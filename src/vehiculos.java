d6040a5a811a00ce524bd376a436191f442859f4
DennisCatana <dennis.joel235@outlook.es>
2023-07-11 09:50:00 -0500

    xd

--- src/vehiculos.java
+++ src/vehiculos.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class vehiculos {
+public class vehiculos {
+    private String marca;
+    private int anio;
+    private String color;
+
+    public vehiculos(String marca, int anio, String color) {
+        this.marca = marca;
+        this.anio = anio;
+        this.color = color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
--- src/vehiculosxd.java
+++ src/vehiculosxd.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class vehiculosxd {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class vehiculosxd {
+    public static ArrayList<vehiculosxd> vehiculos = new ArrayList<>();
+
+    private JPanel vehiculosxd;
+    private JTextField marcax;
+    private JTextField anio;
+    private JTextField color;
+    private JButton GuardarButton;
+    private JButton anteriorButton;
+    private JButton siguienteButton;
+    private JButton MostrarButton;
+
+    public vehiculosxd() {
+        GuardarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String marca = marcax.getText();
+                int aniox = Integer.parseInt(anio.getText());
+                String colorx = color.getText();
+
+                vehiculosxd vehiculo = new vehiculosxd(marca, aniox, colorx);
+                vehiculos.add(vehiculo);
+
+                // Limpiar campos de texto
+                marcax.setText("");
+                anio.setText("");
+                color.setText("");
+            }
+        });
+
+        MostrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Mostrar los detalles de cada vehículo en la consola
+                for (vehiculosxd : vehiculos) {
+                    System.out.println(vehiculo.getMarca() + " " + vehiculo.getAnio() + " " + vehiculo.getColor());
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("vehiculosxd");
+        frame.setContentPane(new vehiculosxd().vehiculosxd);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
